ff7dcd9c38d069c7183a5ac83647a8046bbc5579
Ayusha Upreti <ayusha_upreti@hotmail.com>
2022-09-03 19:23:31 -0400

    first commit

---
+++ lambdas/cognito/verify-auth-challenge-response.js
@@ -0,0 +1,12 @@
+
+exports.main = async (event, context, callback) => {
+    const expectedAnswer = event.request.privateChallengeParameters.secretLoginCode;
+
+    if (event.request.challengeAnswer === expectedAnswer) {
+        event.response.answerCorrect = true;
+    } else {
+        event.response.answerCorrect = false;
+    }
+
+    callback(null, event);
+};
